a0f0c1e9a00aab576d193082cf42aa8930ea8bfd
Lavanya Gudimanchi <srilavanya.gudimanchi@gmail.com[D[D[D[D[D[D[D[D[D[D0817@gma[C[Cail.com>
2022-04-12 22:58:13 -0400

    added jsp

--- src/main/java/teksystems/casestudy/controller/LoginController.java
+++ src/main/java/teksystems/casestudy/controller/LoginController.java
@@ -2,12 +2,15 @@ package teksystems.casestudy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import teksystems.casestudy.database.entity.User;
 import teksystems.casestudy.service.UserService;
 
 @Controller
-public class UserController {
+public class LoginController {
 
     @Autowired
     private UserService userService;
@@ -16,9 +19,16 @@ public class UserController {
     public User getUser(@RequestParam(value = "loginId") String loginId,
                         @RequestParam(value = "password") String password) {
 
-        User user = userService.getUser(loginId,password);
+        User user = userService.validateUser(loginId,password);
 
         return user;
+    }
+
+    @RequestMapping(value = "/login/login", method = RequestMethod.GET)
+    public ModelAndView login() throws Exception {
+        ModelAndView response = new ModelAndView();
+        response.setViewName("login/loginForm");
+        return response;
     }
 
 }
